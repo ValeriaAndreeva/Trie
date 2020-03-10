cebc3b800edd3e02bd42fc39170a72270d004762
andreevavalerya <valeria.andreeva.2013@mail.ru>
2020-03-10 23:27:40 +0300

    Trie

--- src/test/java/TrieTest.java
+++ src/test/java/TrieTest.java
@@ -1,12 +1,35 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import java.util.ArrayList;
 
 class TrieTest {
 
-    @org.junit.jupiter.api.Test
+    private com.company.Trie trie = new com.company.Trie();
+
+    @Test
     void find() {
+        Assert.assertFalse(trie.find("камод"));
+        Assert.assertTrue(trie.find("кот"));
+        Assert.assertTrue(trie.find("ком"));
+        Assert.assertTrue(trie.find("комод"));
+        Assert.assertTrue(trie.find("язык"));
+        Assert.assertTrue(trie.find("язва"));
+        Assert.assertTrue(trie.find("яБлОКо"));
+        Assert.assertFalse(trie.find("кит"));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void prefixFind() {
+    }
+
+    @Before
+    void add() {
+        trie.add("кот");
+        trie.add("ком");
+        trie.add("комод");
+        trie.add("язык");
+        trie.add("язва");
+        trie.add("яБлОКо");
     }
 }
